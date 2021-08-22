f72dcc42d82642d6b1e58da5e54c5484e569b025
Denis Prokhorov <denis.prokhorov@returnonintelligence.com>
2021-08-22 08:40:24 +0300

    Домашка за занятие 16.08 убрал статики из методов, вставил создание экземпляров класса, где требуется.
    добавил enum, с типом организации (или частного лица), который хочет взять кредит
    
    не представляю, что должен возвращать loanResponse (придумал, что возвращаем ответ выдаем кредит или енет и проценты)

---
+++ app/src/main/java/com/tinkoff/edu/app/LoanType.java
@@ -0,0 +1,7 @@
+package com.tinkoff.edu.app;
+
+public enum LoanType {
+    Person,   // кредит на физика
+    LLC,      // кредит на ООО
+    IP        // кредит на ИП
+}
--- app/src/main/java/com/tinkoff/edu/test/LoanCalcTest.java
+++ app/src/main/java/com/tinkoff/edu/test/LoanCalcTest.java
@@ -1,7 +1,10 @@
 package com.tinkoff.edu.test;
 
 import com.tinkoff.edu.app.LoanCalcController;
+import com.tinkoff.edu.app.LoanRequest;
+
 import static org.junit.Assert.assertTrue;
+
 
 /**
  * Hello world!
@@ -11,7 +14,9 @@ public class LoanCalcTest
 {
     public static void main(String... args)
     {
-        int requestId = LoanCalcController.createRequest();
+        LoanRequest request = new LoanRequest(10,1_000);
+        LoanCalcController loanCalcController = new LoanCalcController();
+        int requestId = loanCalcController.createRequest(request);
         assertTrue("requestId must be 1",requestId==1);
     }
 }
