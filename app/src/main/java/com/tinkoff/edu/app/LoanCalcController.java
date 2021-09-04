2eadfba56c5f243b0e91125bc725596666e5c139
Denis Prokhorov <denis.prokhorov@returnonintelligence.com>
2021-09-04 23:05:58 +0300

    Домашка за 19.08 - сделал нормальный DI

--- app/src/main/java/com/tinkoff/edu/app/LoanCalcController.java
+++ app/src/main/java/com/tinkoff/edu/app/LoanCalcController.java
@@ -3,11 +3,17 @@ package com.tinkoff.edu.app;
 
 public class LoanCalcController {
     private LoanCalcLogger loanCalcLogger = new LoanCalcLogger();
-    private LoanCalcService service = new StaticLoanCalcService();
+    private LoanCalcService service;
 
     /**
      *TODO  validates and logs request
      */
+
+    public LoanCalcController(LoanCalcService service){
+        this.service = service;
+    }
+
+
     public int createRequest(LoanRequest request) {
         loanCalcLogger.log(request);
         return service.createRequest(request);
